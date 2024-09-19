e3709463f0daf383342d5aff42d50fe87bfe77ef
Alan <alantronchi@ceroca.cat>
2024-09-19 17:56:32 +0200

    movivemntNau

---
+++ Assets/Scripts/NauJugador.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NauJugador : MonoBehaviour
+{
+    private float _vel;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _vel = 8;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float direccioIndicadaX = Input.GetAxisRaw("Horizontal");
+        float direccioIndicadaY = Input.GetAxisRaw("Vertical");
+        //Debug.Log("X: " + direccioIndicadaX + " - Y: " + direccioIndicadaY);
+
+        Vector2 direccioIndicada = new Vector2(direccioIndicadaX, direccioIndicadaY).normalized;
+
+        Vector2 novaPos = transform.position;
+        novaPos = novaPos + direccioIndicada * _vel * Time.deltaTime;
+        transform.position = novaPos; //tres lineas que hacen que la variable posicion la guardamos en nova, y luego al igualar hacemos que la posicion cambie.
+    }
+}
