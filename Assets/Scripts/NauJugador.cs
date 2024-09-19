fad2828b44888899d67994b5682bf85116410720
Alan <alantronchi@ceroca.cat>
2024-09-19 19:30:54 +0200

    NauControlada

--- Assets/Scripts/NauJugador.cs
+++ Assets/Scripts/NauJugador.cs
@@ -5,11 +5,20 @@ using UnityEngine;
 public class NauJugador : MonoBehaviour
 {
     private float _vel;
-    
+
     // Start is called before the first frame update
+
+    Vector2 minPantalla, maxPantalla;
     void Start()
     {
         _vel = 8;
+        minPantalla = Camera.main.ViewportToWorldPoint(new Vector2(0, 0)); //limite inferior
+        maxPantalla = Camera.main.ViewportToWorldPoint(new Vector2(1, 1)); //limite superior
+
+        minPantalla.x = minPantalla.x + 1.3f;
+        maxPantalla.x = maxPantalla.x - 1.3f;
+        minPantalla.y = minPantalla.y + 1.3f;
+        maxPantalla.y = maxPantalla.y - 1.3f;
     }
 
     // Update is called once per frame
@@ -23,6 +32,10 @@ public class NauJugador : MonoBehaviour
 
         Vector2 novaPos = transform.position;
         novaPos = novaPos + direccioIndicada * _vel * Time.deltaTime;
+
+        novaPos.x = Mathf.Clamp(novaPos.x, minPantalla.x,maxPantalla.x);
+        novaPos.y = Mathf.Clamp(novaPos.y, minPantalla.y,maxPantalla.y);
+       
         transform.position = novaPos; //tres lineas que hacen que la variable posicion la guardamos en nova, y luego al igualar hacemos que la posicion cambie.
     }
 }
