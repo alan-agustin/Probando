14c97c8e95fdf86449e24647651175a7cf65f3c5
Alan <alantronchi@ceroca.cat>
2024-09-30 18:37:10 +0200

    numero cayendo

---
+++ Assets/Scripts/GeneradorNumeros.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneradorNumeros : MonoBehaviour
+{
+    [Serialize] private GameObject prefabNumero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/numero.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class numero : MonoBehaviour
+{
+    private float _vel;
+
+    private Vector2 minPantalla;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _vel = 5f;
+
+        minPantalla = Camera.main.ViewportToWorldPoint(new Vector2 (0, 0));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 novaPos = transform.position;
+        novaPos += new Vector2(0, -1) * _vel * Time.deltaTime;
+        transform.position = novaPos;
+
+        if(transform.position.y < minPantalla.y)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
